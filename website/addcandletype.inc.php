1cfd05376f39b761a3325555b3d3fab5654dc3e3
Arghavan Katebi <Ari.kate128@gmail.com>
2025-12-04 19:16:31 -0500

    comment

--- website/addcandletype.inc.php
+++ website/addcandletype.inc.php
@@ -1,10 +1,8 @@
 <?php
+/*Arghavan  Katebi-12/03/2025-IT202:Internet Applications-Section003-Phase 5 Assignment/ak3426@njit.edu*/
 require_once("candletype.php");
-
 if (isset($_SESSION['login'])) {
-
     $CandleTypeID = filter_input(INPUT_POST, 'CandleTypeID', FILTER_VALIDATE_INT);
-
     if ($CandleTypeID === false || $CandleTypeID === null) {
         echo "<h2>Sorry, you must enter a valid Candle Type ID number</h2>";
     } 
@@ -16,7 +14,6 @@ if (isset($_SESSION['login'])) {
         $CandleTypeCode = htmlspecialchars($_POST['CandleTypeCode']);
         $CandleTypeName = htmlspecialchars($_POST['CandleTypeName']);
         $ShelfNumber    = htmlspecialchars($_POST['ShelfNumber']);
-
         $candleType = new CandleType($CandleTypeID, $CandleTypeCode, $CandleTypeName, $ShelfNumber);
         $result = $candleType->saveCandleType();
 
